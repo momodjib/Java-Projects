2d06ca5a05c38b14464a52ad85893f2fe44123f0
Mohamed Diarra <djibrildiarra@ymail.com>
2019-11-26 19:22:57 -0500

    final update of the main

--- Encryption-Decryption/src/djib/Main/Main.java
+++ Encryption-Decryption/src/djib/Main/Main.java
@@ -1,17 +1,15 @@
 package djib.Main;
-import djib.EncryptionMethod.AlphabetMethod;
-import djib.EncryptionMethod.AsciiMethod;
+
 import djib.EncryptionMethod.MethodPicker;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 
-        MethodPicker picker = new MethodPicker();
-        picker.getDetails(new String[]{"-mode","dec","-key","5","-in","dumb.txt","-out","oooo.txt"});
-        picker.pick();
-        picker.process();
-    }
+		MethodPicker picker = new MethodPicker();
+		picker.getDetails(args);
+		picker.pick();
+		picker.process();
+	}
 }
