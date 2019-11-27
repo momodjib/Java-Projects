0b03c08c40cef760b1f8c62ce4d9427372a958a4
Mohamed Diarra <djibrildiarra@ymail.com>
2019-11-26 19:25:35 -0500

    Final update of Ascii method

--- Encryption-Decryption/src/djib/EncryptionMethod/AsciiMethod.java
+++ Encryption-Decryption/src/djib/EncryptionMethod/AsciiMethod.java
@@ -5,78 +5,78 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class AsciiMethod extends EncryptMethod {
-    private char[] chars;
-    @Override
-    public void encrypt(String message, int key) throws IOException {
+	private char[] chars;
+	@Override
+	public void encrypt(String message, int key) throws IOException {
 
-        if(!fromFile || (fromFile && !message.equals(""))){
-            chars = message.toCharArray();
-            if(!outToFile){
-                for (char item : chars) {
-                    char shiftItem = (char) (item + key);
-                    System.out.print(shiftItem);
-                }
-            }else{
-                FileWriter writer = new FileWriter(new File(out));
-                for (char item : chars) {
-                    char shiftItem = (char) (item + key);
-                    writer.write(shiftItem);
-                }
-                writer.close();
-            }
-        }else{
-            File input = new File(in);
-            chars = getData(input.getName()).toCharArray();
-            if(!outToFile) {
-                for (char item : chars) {
-                    char shiftItem = (char) (item + key);
-                    System.out.print(shiftItem);
-                }
-            }else {
-                FileWriter writer = new FileWriter(new File(out));
-                for (char item : chars) {
-                    char shiftItem = (char) (item + key);
-                    writer.write(shiftItem);
-                }
-                writer.close();
-            }
-        }
-    }
+		if(!fromFile || (fromFile && !message.equals(""))){
+			chars = message.toCharArray();
+			if(!outToFile){
+				for (char item : chars) {
+					char shiftItem = (char) (item + key);
+					System.out.print(shiftItem);
+				}
+			}else{
+				FileWriter writer = new FileWriter(new File(out));
+				for (char item : chars) {
+					char shiftItem = (char) (item + key);
+					writer.write(shiftItem);
+				}
+				writer.close();
+			}
+		}else{
+			File input = new File(in);
+			chars = getData(input.getName()).toCharArray();
+			if(!outToFile) {
+				for (char item : chars) {
+					char shiftItem = (char) (item + key);
+					System.out.print(shiftItem);
+				}
+			}else {
+				FileWriter writer = new FileWriter(new File(out));
+				for (char item : chars) {
+					char shiftItem = (char) (item + key);
+					writer.write(shiftItem);
+				}
+				writer.close();
+			}
+		}
+	}
 
-    @Override
-    public void decrypt(String message, int key) throws IOException {
+	@Override
+	public void decrypt(String message, int key) throws IOException {
 
-        if(!fromFile || (fromFile && !message.equals(""))){
-            chars = message.toCharArray();
-            if(!outToFile){
-                for (char item : chars) {
-                    char shiftItem = (char) (item - key);
-                    System.out.print(shiftItem);
-                }
-            }else{
-                FileWriter writer = new FileWriter(new File(out));
-                for (char item : chars) {
-                    char shiftItem = (char) (item - key);
-                    writer.write(shiftItem);
-                }
-                writer.close();
-            }
-        }else{
-            File input = new File(in);
-            chars = getData(input.getName()).toCharArray();
-            if(!outToFile){
-                for (char item : chars) {
-                    char shiftItem = (char) (item - key);
-                    System.out.print(shiftItem);
-                }
-            }else{
-                FileWriter writer = new FileWriter(new File(out));
-                for (char item : chars) {
-                    char shiftItem = (char) (item - key);
-                    writer.write(shiftItem);
-                }
-                writer.close();
-            }
-        }
-    }
+		if(!fromFile || (fromFile && !message.equals(""))){
+			chars = message.toCharArray();
+			if(!outToFile){
+				for (char item : chars) {
+					char shiftItem = (char) (item - key);
+					System.out.print(shiftItem);
+				}
+			}else{
+				FileWriter writer = new FileWriter(new File(out));
+				for (char item : chars) {
+					char shiftItem = (char) (item - key);
+					writer.write(shiftItem);
+				}
+				writer.close();
+			}
+		}else{
+			File input = new File(in);
+			chars = getData(input.getName()).toCharArray();
+			if(!outToFile){
+				for (char item : chars) {
+					char shiftItem = (char) (item - key);
+					System.out.print(shiftItem);
+				}
+			}else{
+				FileWriter writer = new FileWriter(new File(out));
+				for (char item : chars) {
+					char shiftItem = (char) (item - key);
+					writer.write(shiftItem);
+				}
+				writer.close();
+			}
+		}
+	}
 }
