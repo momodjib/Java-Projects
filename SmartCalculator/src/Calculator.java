fbb17edd4c511b3a7d16ff046cea5eb2542aa381
Mohamed Djibril <djibrildiarra@ymail.com>
2019-12-08 23:19:06 -0500

    added the subtraction and made use of the regexps to validate user input

--- SmartCalculator/src/Calculator.java
+++ SmartCalculator/src/Calculator.java
@@ -1,32 +1,88 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
-        public static void main(String[] args) {
+    private static Scanner scanner = new Scanner(System.in);
 
-            Scanner sc = new Scanner(System.in);
-            int sum = 0;
-            while(sc.hasNext()){
-                String line = sc.nextLine();
-                switch(line){
-                    case "":
-                        break;
-                    case "/exit":
-                        System.out.println("Bye!");
-                        break;
-                    case "/help":
-                        System.out.println("The program calculates the sum of numbers");
-                        break;
-                    default:
-                        String[] operands = line.split(" ");
-                        if (operands[1].isBlank()){
-                            System.out.println(operands[0]);
-                        }else{
-                            for (String s:operands) {
-                                sum += Integer.parseInt(s);
-                            }
-                        }
-                }
+    private static void getInput(Scanner scanner){
+
+        while(scanner.hasNext()){
+            String line = scanner.nextLine();
+            switch(line){
+                case "/exit":
+                    System.out.println("Bye!");
+                    return;
+                case "/help":
+                    System.out.println("The program calculates the sum and subtraction of numbers");
+                    break;
+                default:
+                    processLine(line);
+                    break;
             }
         }
+    }
+
+    private static void processLine(String line){
+
+        String sign = "([\\+ | \\-])*";
+        Pattern pattern = Pattern.compile(sign+"(\\d+\\s+"+sign+"\\s+\\d*)+");
+        Matcher matcher = pattern.matcher(line);
+
+        if (line.matches(sign+"\\d+\\s*")){
+            if(line.charAt(0) == '+'){
+                System.out.println(line.trim().substring(1));
+            }else{
+                System.out.println(line.trim());
+            }
+        }else if(line.matches("\\/w+")){
+            System.out.println("Unknown command");
+        }
+        else if (matcher.matches()){
+            line = line.replaceAll("\\s+"," ");
+            String[] items = line.split(" ");
+            int result = Integer.parseInt(items[0]);
+
+            try{
+                for (int i = 1; i < items.length; i+=2){
+                    String operator = getSign(items[i]);
+                    if(operator.equals("+")){
+                        result += Integer.parseInt(items[i+1]);
+                    }else if (operator.equals("-")){
+                        result -= Integer.parseInt(items[i+1]);
+                    }
+                }
+            }catch(NullPointerException e){
+                System.out.println(result);
+            }
+                System.out.println(result);
+        }else{
+            System.out.println("Invalid expression");
+        }
+    }
+
+    private static String getSign(String sign){
+
+        String temp = String.valueOf(sign.charAt(0));
+        for (int i = 1; i < sign.length(); i++) {
+            temp += sign.charAt(i);
+
+            switch (temp){
+                case "++":
+                case "--":
+                    temp = "+";
+                    break;
+                case "+-":
+                case "-+":
+                    temp = "-"; 
+                    break;
+            }
+        }
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        getInput(scanner);
+    }
 }
