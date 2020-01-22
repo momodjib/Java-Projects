000fca3eee7ada2bcfac523fbde5488c2fe46205
Mohamed Djibril <djibrildiarra@ymail.com>
2020-01-21 22:19:03 -0500

    stage 2 in progress

---
+++ GameOfLife/src/GOLStage2/GOLStage2.java
@@ -0,0 +1,30 @@
+package GOLStage2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GOLStage2 {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String[][] currentGeneration;
+
+    private static void generateGame(int size, int seed){
+        Random random = new Random(seed);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (random.nextBoolean()) {
+                    System.out.print("O");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        generateGame(n,s);
+    }
+}
---
+++ GameOfLife/src/GOLStage2/GenerationMaker.java
@@ -0,0 +1,84 @@
+package GOLStage2;
+
+public class GenerationMaker {
+    private static String[][] presentGeneration;
+    private static String[][] nextGeneration;
+
+
+    public String[][] nextGeneration(String[][] generation){
+
+       presentGeneration = generation;
+       int size = presentGeneration.length;
+       nextGeneration = new String[size][size];
+
+       for (int i = 0; i < size; i++) {
+           for (int j = 0; j < size; j++) {
+
+               if(i == 0){
+
+               }else if(i == size-1){
+
+               }else{
+                   if (j == 0) {
+
+                   }else{
+                       int aliveCells = checkNeighbours(i,j);
+                       if (isAlive(i,j)){
+                           if ((aliveCells >=2) && (aliveCells <=3)){
+                               nextGeneration[i][j] = presentGeneration[i][j];
+                           }else{
+                               nextGeneration[i][j] = " ";
+                           }
+                       }
+                       else{
+                           if (aliveCells == 3){
+                               nextGeneration[i][j] = "O";
+                           }else{
+                               nextGeneration[i][j] = presentGeneration[i][j];
+                           }
+                       }
+                   }
+               }
+           }
+       }
+       return nextGeneration;
+    }
+
+   public int checkNeighbours(int row, int col){
+       int aliveNeighbours = 0;
+
+       if (isAlive(row-1,col-1)){
+           aliveNeighbours++;
+       }
+        if (isAlive(row-1,col)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row-1,col+1)){
+            aliveNeighbours++;
+        }
+
+        if (isAlive(row,col-1)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row,col+1)){
+            aliveNeighbours++;
+        }
+
+        if (isAlive(row+1,col-1)){
+            aliveNeighbours++;
+        }
+       if (isAlive(row+1,col)){
+           aliveNeighbours++;
+       }
+       if (isAlive(row-1,col+1)){
+           aliveNeighbours++;
+       }
+
+       return aliveNeighbours;
+    }
+
+   public boolean isAlive(int row, int col){
+       return presentGeneration[row][col].equals("O");
+    }
+
+}
