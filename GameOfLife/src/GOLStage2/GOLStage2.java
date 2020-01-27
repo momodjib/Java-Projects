40ba25dff53c22504becc3a9f34dcaa823106e21
Mohamed Djibril <djibrildiarra@ymail.com>
2020-01-26 21:38:12 -0500

    Stage 2 completed

--- GameOfLife/src/GOLStage2/GOLStage2.java
+++ GameOfLife/src/GOLStage2/GOLStage2.java
@@ -6,25 +6,40 @@ import java.util.Scanner;
 public class GOLStage2 {
     private static Scanner scanner = new Scanner(System.in);
     private static String[][] currentGeneration;
+    private static GenerationMaker game = new GenerationMaker();
 
-    private static void generateGame(int size, int seed){
+    private static void startGame(int size, int seed, int depth){
+        currentGeneration = new String[size][size];
         Random random = new Random(seed);
+        
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (random.nextBoolean()) {
-                    System.out.print("O");
+                    currentGeneration[i][j] = "O";
                 } else {
-                    System.out.print(" ");
+                    currentGeneration[i][j] = " ";
                 }
+            }
+        }
+        for (int i = 1; i <= depth; i++) {
+            currentGeneration = game.nextGeneration(currentGeneration);
+        }
+        showResult();
+
+    }
+
+    public static void showResult(){
+        for (int i = 0; i < currentGeneration.length; i++) {
+            for (int j = 0; j < currentGeneration.length; j++) {
+                System.out.print(currentGeneration[i][j]);
             }
             System.out.println();
         }
     }
-
-
     public static void main(String[] args) {
         int n = scanner.nextInt();
         int s = scanner.nextInt();
-        generateGame(n,s);
+        int m = scanner.nextInt();
+        startGame(n,s,m);
     }
 }
--- GameOfLife/src/GOLStage2/GenerationMaker.java
+++ GameOfLife/src/GOLStage2/GenerationMaker.java
@@ -1,46 +1,35 @@
 package GOLStage2;
 
 public class GenerationMaker {
-    private static String[][] presentGeneration;
+    private static String[][] currentGeneration;
     private static String[][] nextGeneration;
+    private static int aliveNeighbours;
 
 
     public String[][] nextGeneration(String[][] generation){
 
-       presentGeneration = generation;
-       int size = presentGeneration.length;
+       currentGeneration = generation;
+       int size = currentGeneration.length;
        nextGeneration = new String[size][size];
 
        for (int i = 0; i < size; i++) {
            for (int j = 0; j < size; j++) {
 
                if(i == 0){
+                   setNextGeneration(checkTopNeighbours(i,j),i,j);
 
                }else if(i == size-1){
+                   setNextGeneration(checkBottomNeighbours(i,j),i,j);
 
                }else{
                    if (j == 0) {
-
+                        setNextGeneration(checkLeftNeighbours(i,j),i,j);
                    }
                    else if (j == size-1){
-
+                       setNextGeneration(checkRightNeighbours(i,j),i,j);
                    }
                    else{
-                       int aliveCells = checkNeighbours(i,j);
-                       if (isAlive(i,j)){
-                           if ((aliveCells >=2) && (aliveCells <=3)){
-                               nextGeneration[i][j] = presentGeneration[i][j];
-                           }else{
-                               nextGeneration[i][j] = " ";
-                           }
-                       }
-                       else{
-                           if (aliveCells == 3){
-                               nextGeneration[i][j] = "O";
-                           }else{
-                               nextGeneration[i][j] = presentGeneration[i][j];
-                           }
-                       }
+                       setNextGeneration(checkNeighbours(i,j),i,j);
                    }
                }
            }
@@ -48,8 +37,8 @@ public class GenerationMaker {
        return nextGeneration;
     }
 
-   public int checkNeighbours(int row, int col){
-       int aliveNeighbours = 0;
+    public int checkNeighbours(int row, int col){
+        aliveNeighbours = 0;
 
        if (isAlive(row-1,col-1)){
            aliveNeighbours++;
@@ -74,15 +63,299 @@ public class GenerationMaker {
        if (isAlive(row+1,col)){
            aliveNeighbours++;
        }
-       if (isAlive(row-1,col+1)){
+       if (isAlive(row+1,col+1)){
            aliveNeighbours++;
        }
 
        return aliveNeighbours;
     }
 
-   public boolean isAlive(int row, int col){
-       return presentGeneration[row][col].equals("O");
+    public int checkTopNeighbours(int row, int col){
+        aliveNeighbours = 0;
+
+        if (col == 0) {
+            //top neighbours
+            if (isAlive(currentGeneration.length-1,currentGeneration.length-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(currentGeneration.length-1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(currentGeneration.length-1,col+1)){
+                aliveNeighbours++;
+            }
+
+            //direct neighbours
+            if (isAlive(row,currentGeneration.length-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row,col+1)){
+                aliveNeighbours++;
+            }
+
+            //bottom neighbours
+            if (isAlive(row+1,currentGeneration.length-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row+1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row+1,col+1)){
+                aliveNeighbours++;
+            }
+        }
+        else if (col == currentGeneration.length-1){
+
+            //top neighbours
+            if (isAlive(currentGeneration.length-1,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(currentGeneration.length-1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(currentGeneration.length-1,0)){
+                aliveNeighbours++;
+            }
+
+            //direct neighbours
+            if (isAlive(row,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row,0)){
+                aliveNeighbours++;
+            }
+
+            //bottom neighbours
+            if (isAlive(row+1,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row+1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row+1,0)){
+                aliveNeighbours++;
+            }
+        }
+        else{
+            //top neighbours
+            if (isAlive(currentGeneration.length-1,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(currentGeneration.length-1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(currentGeneration.length-1,col+1)){
+                aliveNeighbours++;
+            }
+
+            //direct neighbours
+            if (isAlive(row,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row,col+1)){
+                aliveNeighbours++;
+            }
+
+            //bottom neighbours
+            if (isAlive(row+1,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row+1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row+1,col+1)){
+                aliveNeighbours++;
+            }
+        }
+        return aliveNeighbours;
+    }
+
+    public int checkBottomNeighbours(int row, int col){
+        aliveNeighbours = 0;
+
+        if (col == 0) {
+            //top neighbours
+            if (isAlive(row-1,currentGeneration.length-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row-1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row-1,col+1)){
+                aliveNeighbours++;
+            }
+
+            //direct neighbours
+            if (isAlive(row,currentGeneration.length-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row,col+1)){
+                aliveNeighbours++;
+            }
+
+            //bottom neighbours
+            if (isAlive(0,currentGeneration.length-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(0,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(0,col+1)){
+                aliveNeighbours++;
+            }
+        }
+        else if (col == currentGeneration.length-1){
+
+            //top neighbours
+            if (isAlive(row-1,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row-1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row-1,0)){
+                aliveNeighbours++;
+            }
+
+            //direct neighbours
+            if (isAlive(row,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row,0)){
+                aliveNeighbours++;
+            }
+
+            //bottom neighbours
+            if (isAlive(0,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(0,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(0,0)){
+                aliveNeighbours++;
+            }
+        }
+        else{
+            //top neighbours
+            if (isAlive(row-1,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row-1,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row-1,col+1)){
+                aliveNeighbours++;
+            }
+
+            //direct neighbours
+            if (isAlive(row,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(row,col+1)){
+                aliveNeighbours++;
+            }
+
+            //bottom neighbours
+            if (isAlive(0,col-1)){
+                aliveNeighbours++;
+            }
+            if (isAlive(0,col)){
+                aliveNeighbours++;
+            }
+            if (isAlive(0,col+1)){
+                aliveNeighbours++;
+            }
+        }
+        return aliveNeighbours;
+    }
+
+    public int checkLeftNeighbours(int row, int col){
+        aliveNeighbours = 0;
+
+        if (isAlive(row-1,currentGeneration.length-1)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row-1,col)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row-1,col+1)){
+            aliveNeighbours++;
+        }
+
+        if (isAlive(row,currentGeneration.length-1)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row,col+1)){
+            aliveNeighbours++;
+        }
+
+        if (isAlive(row+1,currentGeneration.length-1)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row+1,col)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row+1,col+1)){
+            aliveNeighbours++;
+        }
+
+        return aliveNeighbours;
+    }
+
+    public int checkRightNeighbours(int row,int col){
+        aliveNeighbours = 0;
+
+        if (isAlive(row-1,col-1)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row-1,col)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row-1,0)){
+            aliveNeighbours++;
+        }
+
+        if (isAlive(row,col-1)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row,0)){
+            aliveNeighbours++;
+        }
+
+        if (isAlive(row+1,col-1)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row+1,col)){
+            aliveNeighbours++;
+        }
+        if (isAlive(row+1,0)){
+            aliveNeighbours++;
+        }
+
+        return aliveNeighbours;
+    }
+
+    public void setNextGeneration(int aliveCells, int row, int col){
+
+        if (isAlive(row,col)){
+            if ((aliveCells == 2) || (aliveCells == 3)){
+                nextGeneration[row][col] = "O";
+            }else{
+                nextGeneration[row][col] = " ";
+            }
+        }
+        else{
+            if (aliveCells == 3){
+                nextGeneration[row][col] = "O";
+            }else{
+                nextGeneration[row][col] = " ";
+            }
+        }
+    }
+
+    public boolean isAlive(int row, int col){
+       return currentGeneration[row][col].equals("O");
     }
 
 }
